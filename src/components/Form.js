0d21dc14462ef673af69319e50bd0a8abe0d1de0
totquf <dalda117@naver.com>
2023-03-11 00:35:56 +0900

    useRef

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styled from "styled-components";
 
 const StyledTextInput = styled.TextInput.attrs({
@@ -17,11 +17,17 @@ const StyledText = styled.Text`
     margin: 10px;
     `
 const Form = () => {
+
+    //state
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
+    const refName = useRef(null)
+    const refEmail = useRef(null)
 
     useEffect(() => {
         console.log('컴포넌트 마운트 됨')
+        //컴포넌트가 마운트될때 refName컴포넌트를 포커스
+        refName.current.focus()
         return () => console.log('컴포넌트 언마운트 됨')
     },[])
 
@@ -34,12 +40,20 @@ const Form = () => {
                 value={name}
                 onChangeText={(text) => setName(text)}
                 placeholder="이름을 입력하세요"
+
+                ref={refName}
+                returnKeyType="next" //키보드 버튼이름 다음으로 변경
+                //입력이 완료되면 refEmail컴포넌트를 포커스
+                onEndEditing={() => refEmail.current.focus()}
             />
 
             <StyledTextInput
                 value={email}
                 onChangeText={(text) => setEmail(text)}
                 placeholder="이메일을 입력하세요"
+
+                ref={refEmail}
+                returnKeyType="done" //키보드 버튼이름 다음으로 변경
             />
         </>
     )
